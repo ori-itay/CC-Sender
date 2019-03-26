9fb4d5fd317ab7d0ac40bdeda8f302bf9f37389b
itaylevi1 <39258073+itaylevi1@users.noreply.github.com>
2019-03-26 19:05:08 +0200

    divided to sub functions

--- Sender.c
+++ Sender.c
@@ -1,9 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "winsock2.h" 
 #include <ws2tcpip.h>
 #include <math.h>
+#include <windows.h>
+#include <process.h>
+
 
 #pragma comment(lib, "Ws2_32.lib")
 
@@ -13,15 +17,15 @@
 #define BYTE_SIZE 8
 
 void Init_Winsock();
-void compute_block(char read_buff[READ_BUFF], char s_c_buff_1[S_C_BUFF]);
+int get_file_size(FILE *fp);
+int read_from_file(FILE *fp, char file_read_buff[]);
+void compute_frame(char read_buff[READ_BUFF], char s_c_buff_1[S_C_BUFF]);
+void send_frame(char s_c_buff_1[], int s_c_fd, struct sockaddr_in cnl_addr, int num_to_write);
+DWORD WINAPI thread_end_listen(void *param);
 
 
 int END_FLAG = 0;
-
-struct ARGS {
-	int socket_fd;
-	int buff[RECEIVE_BUFF];
-};
+int recv_buff[RECEIVE_BUFF];
 
 int main(int argc, char** argv) {
 
@@ -29,7 +33,7 @@ int main(int argc, char** argv) {
 
 	char s_c_buff_1[S_C_BUFF], file_read_buff[READ_BUFF];
 	int recv_buff[RECEIVE_BUFF];
-	int chnl_port = -1, totalsent = -1, num_sent = -1, num_read = -1, notwritten = -1,
+	int chnl_port = -1, totalsent = -1, num_sent = -1, num_read = -1, num_to_write = -1,
 		not_been_read = -1, input_file_size = 0, s_c_fd = -1;
 	FILE *fp;
 
@@ -43,7 +47,7 @@ int main(int argc, char** argv) {
 		fprintf(stderr, "Error: not able to open file. \n");
 		exit(1);
 	}
-	input_file_size = get_file_size(file_name);
+	input_file_size = get_file_size(fp);
 
 
 	//sender - channel socket
@@ -58,33 +62,14 @@ int main(int argc, char** argv) {
 	cnl_addr.sin_port = htons(chnl_port);
 	cnl_addr.sin_addr.s_addr = inet_addr(ip);
 
-	struct ARGS thread_args = { s_c_fd , recv_buff };
-	HANDLE thread = CreateThread(NULL, 0, thread_end_listen, &thread_args, 0, NULL);
+	HANDLE thread = CreateThread(NULL, 0, thread_end_listen, &s_c_fd, 0, NULL);
 
 	not_been_read = input_file_size;
 	while (not_been_read > 0 && END_FLAG == 0) {
-		memset(file_read_buff, 0, READ_BUFF);
-		num_read = read(fp, file_read_buff, READ_BUFF);
-		if (num_read <= 0) {
-			fprintf(stderr, "Error reading fron file. exiting...\n");
-			exit(1);
-		}
-		not_been_read -= num_read;
-		compute_block(file_read_buff, s_c_buff_1);
-
-		notwritten = num_read; //curr num of bytes to write
-		totalsent = 0;
-		// keep looping until nothing left to write for this BUFF size block
-		while (notwritten > 0 && END_FLAG == 0) {
-			// notwritten = how much left to write ; totalsent = how much written so far ; num_sent = how much written in last write() call
-			num_sent = sendto(s_c_fd, s_c_buff_1 + totalsent, notwritten, 0, (SOCKADDR*)&cnl_addr, sizeof(cnl_addr));
-			if (num_sent == -1) {// check if error occured (server closed connection?)
-				fprintf(stderr, "%s\n", strerror(errno));
-				exit(1);
-			}
-			totalsent += num_sent;
-			notwritten -= num_sent;
-		}
+		num_to_write = read_from_file(fp, file_read_buff);; //curr num of bytes to write
+		not_been_read -= num_to_write;
+		compute_frame(file_read_buff, s_c_buff_1);
+		send_frame(s_c_buff_1, s_c_fd, cnl_addr, num_to_write);
 	}
 	printf("received: %d bytes\nwritten: %d bytes\ndetected: %d errors, corrected: %d errors\n",
 		recv_buff[0], recv_buff[1], recv_buff[2], recv_buff[3]);
@@ -107,7 +92,6 @@ void Init_Winsock() {
 }
 
 
-
 int get_file_size(FILE *fp) {
 	int size = 0;
 
@@ -118,7 +102,8 @@ int get_file_size(FILE *fp) {
 	return size;
 }
 
-void compute_block(char read_buff[READ_BUFF], char s_c_buff_1[S_C_BUFF]) {
+
+void compute_frame(char read_buff[READ_BUFF], char s_c_buff_1[S_C_BUFF]) {
 
 	int bit_ind, read_ind, write_ind, block_ind, xor = 0, bit_pos, i, j;
 	char curr_bit;
@@ -171,31 +156,57 @@ void compute_block(char read_buff[READ_BUFF], char s_c_buff_1[S_C_BUFF]) {
 }
 
 
-
 DWORD WINAPI thread_end_listen(void *param) {
 
-	int status, bytes_read, notread, totalread, num_sent;
-	int recv_buff[] = ((struct ARGS*)param)->buff;
-	int s_c_fd = ((struct ARGS*)param)->socket_fd;
+	int status, bytes_read, notread;
+	int s_c_fd = *(int*)(param);
+
 
 	while (END_FLAG == 0) {
 		notread = RECEIVE_BUFF;
 		bytes_read = 0;
 		while (notread > 0) {
-			bytes_read = recvfrom(s_c_fd, recv_buff + bytes_read, RECEIVE_BUFF, 0, 0, 0); //deliver fd and buff as parameter or as global vars
+			bytes_read = recvfrom(s_c_fd, (char*)recv_buff + bytes_read, RECEIVE_BUFF*sizeof(int), 0, 0, 0);
 			if (bytes_read == -1) {
 				fprintf(stderr, "%s\n", strerror(errno));
-				_endthread(1);
+				exit(1);
 			}
-			totalread += bytes_read;
-			notread -= num_sent;
+			notread -= bytes_read;
 		}
 		status = shutdown(s_c_fd, SD_BOTH);
 		if (status) {
 			printf("Error while closing socket. \n");
-			_endthread(1);
+			return 1;
 		}
 		END_FLAG = 1;
 	}
-	_endthread(0);
+	return 0;
+}
+
+
+int read_from_file(FILE *fp, char file_read_buff[]) {
+	int num_read = -1;
+
+	memset(file_read_buff, 0, READ_BUFF);
+	num_read = fread(file_read_buff, sizeof(char), READ_BUFF, fp);
+	if (num_read <= 0) {
+		fprintf(stderr, "Error reading fron file. exiting...\n");
+		exit(1);
+	}
+	return num_read;
+}
+
+
+void send_frame(char s_c_buff_1[], int s_c_fd, struct sockaddr_in cnl_addr, int num_to_write) {
+	int totalsent = 0, num_sent = 0;
+
+	while (num_to_write > 0 && END_FLAG == 0) {
+		num_sent = sendto(s_c_fd, s_c_buff_1 + totalsent, num_to_write, 0, (SOCKADDR*)&cnl_addr, sizeof(cnl_addr));
+		if (num_sent == -1) {
+			fprintf(stderr, "%s\n", strerror(errno));
+			exit(1);
+		}
+		totalsent += num_sent;
+		num_to_write -= num_sent;
+	}
 }
