34ddb67b57896e068a98511ea568eff176551f39
itaylevi1 <39258073+itaylevi1@users.noreply.github.com>
2019-03-27 18:13:39 +0200

    final before any checks

--- Sender.c
+++ Sender.c
@@ -9,74 +9,72 @@
 #include <windows.h>
 #include <process.h>
 
-
 #pragma comment(lib, "Ws2_32.lib")
 
-#define RECEIVE_BUFF 4
-#define S_C_BUFF 64
+#define UDP_BUFF 64
 #define READ_BUFF 49
-#define BYTE_SIZE 8
 
 void Init_Winsock();
 int get_file_size(FILE *fp);
 int read_from_file(FILE *fp, char file_read_buff[]);
-void compute_frame(char read_buff[READ_BUFF], char s_c_buff_1[S_C_BUFF]);
+void compute_frame(char read_buff[READ_BUFF], char s_c_buff_1[UDP_BUFF]);
 void send_frame(char buff[], int fd, struct sockaddr_in to_addr, int bytes_to_write);
 void receive_frame(char buff[], int fd, int bytes_to_read);
 DWORD WINAPI thread_end_listen(void *param);
 
-
 int END_FLAG = 0;
-int recv_buff[RECEIVE_BUFF];
 
 int main(int argc, char** argv) {
 
 	Init_Winsock();
 
-	char s_c_buff_1[S_C_BUFF], file_read_buff[READ_BUFF];
-	int recv_buff[RECEIVE_BUFF];
-	int chnl_port = -1, totalsent = -1, num_sent = -1, num_read = -1, num_to_write = -1,
-		not_been_read = -1, input_file_size = 0, s_c_fd = -1;
+	char udp_buff[UDP_BUFF], file_read_buff[READ_BUFF];
+	int recv_buff[UDP_BUFF];
+	int chnl_port = -1, not_been_read = -1, input_file_size = 0, s_c_fd = -1;
 	FILE *fp;
+	struct sockaddr_in chnl_addr;
 
 	if (argc != 4) {
 		fprintf(stderr, "Error: not enough arguments were provided\n");
 		exit(1);
 	}
-	char* ip = argv[1]; char* port = argv[2]; char* file_name = argv[3];
+	char* chnl_ip = argv[1]; char* port = argv[2]; char* file_name = argv[3];
 
-	if ((fp = fopen(file_name, "r")) == NULL) {
+	if ((fp = fopen(file_name, "rb")) == NULL) {
 		fprintf(stderr, "Error: not able to open file. \n");
 		exit(1);
 	}
 	input_file_size = get_file_size(fp);
-
 
 	//sender - channel socket
 	if ((s_c_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		fprintf(stderr, "%s\n", strerror(errno));
 		exit(1);
 	}
+	//channel address
 	chnl_port = (unsigned int)strtoul(port, NULL, 10); //get channel's port number
-	struct sockaddr_in cnl_addr;
-	memset(&cnl_addr, 0, sizeof(cnl_addr));
-	cnl_addr.sin_family = AF_INET;
-	cnl_addr.sin_port = htons(chnl_port);
-	cnl_addr.sin_addr.s_addr = inet_addr(ip);
+	memset(&chnl_addr, 0, sizeof(chnl_addr));
+	chnl_addr.sin_family = AF_INET;
+	chnl_addr.sin_port = htons(chnl_port);
+	chnl_addr.sin_addr.s_addr = inet_addr(chnl_ip);
 
-	if (bind(s_c_fd, (SOCKADDR*) &cnl_addr, sizeof(cnl_addr)) < 0) {
+	if (bind(s_c_fd, (SOCKADDR*) &chnl_addr, sizeof(chnl_addr)) < 0) {
 		fprintf(stderr, "%s\n", strerror(errno));
 		exit(1);
 	}
 
 	HANDLE thread = CreateThread(NULL, 0, thread_end_listen, &s_c_fd, 0, NULL);
+
 	not_been_read = input_file_size;
 	while (not_been_read > 0 && END_FLAG == 0) {
-		num_to_write = read_from_file(fp, file_read_buff); //curr num of bytes to write
-		not_been_read -= num_to_write;
-		compute_frame(file_read_buff, s_c_buff_1);
-		send_frame(s_c_buff_1, s_c_fd, cnl_addr, num_to_write);
+		read_from_file(fp, file_read_buff);
+		not_been_read -= READ_BUFF;
+		compute_frame(file_read_buff, udp_buff);
+		send_frame(udp_buff, s_c_fd, chnl_addr, UDP_BUFF);
 	}
+
+	while (END_FLAG == 0) {}
+	receive_frame((char*)recv_buff, s_c_fd, UDP_BUFF); //receive stats from channel
 	printf("received: %d bytes\nwritten: %d bytes\ndetected: %d errors, corrected: %d errors\n",
 		recv_buff[0], recv_buff[1], recv_buff[2], recv_buff[3]);
 
@@ -112,12 +110,12 @@ int get_file_size(FILE *fp) {
 }
 
 
-void compute_frame(char read_buff[READ_BUFF], char s_c_buff_1[S_C_BUFF]) {
+void compute_frame(char read_buff[READ_BUFF], char udp_buff[UDP_BUFF]) {
 
 	int bit_ind, read_ind, write_ind, block_ind, xor = 0, bit_pos, i, j;
 	char curr_bit;
 
-	memset(s_c_buff_1, 0, S_C_BUFF);
+	memset(udp_buff, 0, UDP_BUFF);
 
 	for (block_ind = 0; block_ind < 8; block_ind++) {
 		printf("old block no # %d \n", block_ind); //print - delete after!!!!!!!!!!!!
@@ -127,7 +125,7 @@ void compute_frame(char read_buff[READ_BUFF], char s_c_buff_1[S_C_BUFF]) {
 
 			if ((bit_ind % 7) == 0 && bit_ind != 0) {
 				//store parity
-				s_c_buff_1[write_ind] = xor | s_c_buff_1[write_ind];
+				udp_buff[write_ind] = xor | udp_buff[write_ind];
 				xor = 0;
 			}
 
@@ -136,7 +134,7 @@ void compute_frame(char read_buff[READ_BUFF], char s_c_buff_1[S_C_BUFF]) {
 			bit_pos = 7 - bit_ind % 7;
 
 			curr_bit = (read_buff[read_ind] & ((int)pow(2, bit_pos))) != 0; // 1 if result after mask is different from 0. otherwise - 0.
-			s_c_buff_1[write_ind] = (curr_bit << bit_pos) | s_c_buff_1[write_ind];
+			udp_buff[write_ind] = (curr_bit << bit_pos) | udp_buff[write_ind];
 			xor ^= curr_bit;
 
 			printf("%d", curr_bit);		//print - delete after!!!!!!!!!!!!
@@ -144,7 +142,7 @@ void compute_frame(char read_buff[READ_BUFF], char s_c_buff_1[S_C_BUFF]) {
 		printf("\n");//print - delete after!!!!!!!!!!!!
 
 		for (i = 0; i < 7; i++) {
-			s_c_buff_1[7 + (block_ind * 8)] ^= s_c_buff_1[i];
+			udp_buff[7 + (block_ind * 8)] ^= udp_buff[i];
 		}
 		printf("\n------------\n");//print - delete after!!!!!!!!!!!!
 	}
@@ -156,7 +154,7 @@ void compute_frame(char read_buff[READ_BUFF], char s_c_buff_1[S_C_BUFF]) {
 		}
 
 		for (j = 0; j < 8; j++) {//print - delete after!!!!!!!!!!!!
-			printf("%d", !!((s_c_buff_1[i] << j) & 0x80));//print - delete after!!!!!!!!!!!!
+			printf("%d", !!((udp_buff[i] << j) & 0x80));//print - delete after!!!!!!!!!!!!
 		}//print - delete after!!!!!!!!!!!!
 		printf("\n");//print - delete after!!!!!!!!!!!!
 	}
@@ -171,13 +169,12 @@ DWORD WINAPI thread_end_listen(void *param) {
 	int s_c_fd = *(int*)(param);
 
 	while (END_FLAG == 0) {
-		receive_frame((char*)recv_buff, s_c_fd, RECEIVE_BUFF * sizeof(int));
+		END_FLAG = 1;
 		status = shutdown(s_c_fd, SD_SEND);
 		if (status) {
 			printf("Error while closing socket. \n");
 			return 1;
 		}
-		END_FLAG = 1;
 	}
 	return 0;
 }
@@ -188,7 +185,7 @@ int read_from_file(FILE *fp, char file_read_buff[]) {
 
 	memset(file_read_buff, 0, READ_BUFF);
 	num_read = fread(file_read_buff, sizeof(char), READ_BUFF, fp);
-	if (num_read <= 0) {
+	if (num_read < 0) {
 		fprintf(stderr, "Error reading fron file. exiting...\n");
 		exit(1);
 	}
